966c289bde64bb45bced8c50d84048e934f07ace
Juan Nunez <51241293+nunju9a@users.noreply.github.com>
2019-08-07 18:55:15 -0400

    updated navlinks

--- Nav.js
+++ Nav.js
@@ -1,22 +1,14 @@
-import React from 'react';
+import React from "react";
+import {NavLink} from "react-router-dom";
 
-import {
-    NavLink
-} from 'react-router-dom';
-
-const Nav =(props) => (
-    <nav className='main-nav'>
-      <ul>
-        <li>
-           <NavLink exact to ='/buttonA'>{props.buttonText[0]}</Navlink>
-        </li>
-        <li>
-           <NavLink exact to ='/buttonB'>{props.buttonText[1]}</Navlink>
-        </li>
-        <li>
-           <NavLink exact to ='/buttonC'>{props.buttonText[2]}</Navlink>
-        </li>
-      </ul>
+// RENDERING NAVLINKS FOR ROUTES
+const Nav = () => (
+    <nav className="main-nav">
+        <ul>
+            <li> <NavLink to="/puppies"> Puppies </NavLink> </li>
+            <li> <NavLink to="/sunsets"> Sunsets </NavLink> </li>
+            <li> <NavLink to="/rollercoasters"> Roller Coasters </NavLink> </li>
+        </ul>
     </nav>
 );
 
