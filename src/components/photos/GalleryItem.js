2f4ab5bea38e3b30cd8b6c32b01961b7bcd1ee9f
Juan Nunez <51241293+nunju9a@users.noreply.github.com>
2019-08-08 09:27:27 -0400

    created GalleryItem.js

---
+++ src/components/photos/GalleryItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const GalleryItem = props => {
+    return (
+        <li>
+            <img src={props.url} alt="" />
+        </li>
+    );
+}
+export default GalleryItem;
